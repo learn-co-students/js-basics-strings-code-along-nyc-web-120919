5a1c4b789c87dccf64b29728b971472b2c50407d
github username <github email address>
2020-01-25 23:11:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'eunice';
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+// let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+// let shortGreeting = "Welcome, ${currentUser}.charAt(0)!";
+// let shortGreeting = "Welcome, ${currentUser}.slice(0, 1)!";
+let shortGreeting = "Welcome, ${currentUser.slice(0, 1)}!";
