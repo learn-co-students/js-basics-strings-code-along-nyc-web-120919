d6111dd76ff52ee8d4c0eef9036215cf5664a211
Alisher <Allumati@icloud.com>
2020-01-27 15:45:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+// Write your code in this file
+let currentUser = "Extraordinarii"
+let welcomeMessage = "Welcome to Flatbook,"+ " " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK," + " " + currentUser.toUpperCase() + "!"
+let shortGreeting = "Welcome, " + currentUser[0] + "!"
